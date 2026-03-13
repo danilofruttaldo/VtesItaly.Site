cb732b064878b0f5857ec2375a28c586ba1e3820
Danilo Fruttaldo <danilo.fruttaldo@mbsengineering.it>
2026-03-13 09:58:55 +0100

    Rename /blog/ to /comunita/ and /en/community/ with redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,5 +18,8 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
-  redirects: {},
+  redirects: {
+    '/blog': '/comunita',
+    '/en/blog': '/en/community',
+  },
 });
